a35a70e30ce2e0f34ee84baed642f4a54d13012d
TmLololol <tmtanky@gmail.com>
2021-10-20 22:32:00 +0800

    Convert styles that uses props to object syntax and added 1 space for every styled components

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -1,21 +1,21 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components";
 
+export const GlobalStyles = createGlobalStyle(({theme}) => ({
+  'html, body': {
+    overflowX: 'hidden',
+    backgroundColor: theme.body,
+    color: theme.text,
+    fontFamily: 'Nunito Sans, sans-serif'
+  },
 
-export const GlobalStyles = createGlobalStyle`
-  html,body {
-    overflow-x: hidden;
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-    font-family: 'Nunito Sans', sans-serif;
+  'a': {
+    color: 'inherit',
+    textDecoration: 'none'
+  },
+
+  '*': {
+    padding: '0',
+    margin: '0',
+    boxSizing: 'border-box'
   }
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  
-  * {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-  `
+}))
