c5f74e45696ab17f972cd1f28b5b139c6b9656f4
TmLololol <tmtanky@gmail.com>
2021-10-26 22:45:33 +0800

    deleted the stringManipulator.ts in utilities/functions folder

--- src/utilities/functions/stringManipulator.function.ts
+++
@@ -1,9 +0,0 @@
-export const arrayToString = (array: any[]) => {
-    return array.map((item: string, index) => {
-        if(array.length !== index + 1) {
-            return `${item}, `
-        } else {
-            return `${item}`
-        }
-    })
-}
